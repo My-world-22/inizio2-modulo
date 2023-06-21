479625f815c484aa67fa2ced95e69ae39b50f2bc
My-world-22 <130664590+My-world-22@users.noreply.github.com>
2023-06-21 10:11:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// Esercizio di inizio settimana! {RISPOSTE}
+
+// Cosè un Algoritmo? 
+
+// Un ALGORITMO possiamo definirlo in parole semplici {SPERO!} > Un insieme di istruzioni specifiche 
+// che bisogna seguire alla lettera per poter portare a termine un determinato compito. Come per esempio: 
+// leggere il libretto di montaggio di un mobile, se si seguono alla lettera le determinnate istruzioni, 
+// il mobile sarà assemblato correttamente e non dovrremmo temere che il mobile ricada in pezzi su se stesso! (-;
+
+
+// Cosè una Variabile?
+
+// Possiame definire una VARIABILE come un contenitore, dove al suo interno possiamo porre uno o piu ogetti da 
+// richiamare all'occorrenza. Questi contenitori {VARIABILI} possiamo riempirli, svuotarli, modificarne il contenuto 
+// all'occorenza. Ogni contenitore {VARIABILE} deve avere un etichetta nominativa unica come 
+// nel caso in questione{LINGUAGGIO JAVASCRIPT= CASE SENSITIVE} un contnitore rinominato > "fruste" -- è differente 
+// da un contenitore rinominato > "fruSte" .
+
+                          
+
+// UNDEFINED & NULL
+
+// CIOTOLA CON AL SUO INTERNO IL SUO CONTENUTO {VARIABILE DICHIARATA E DEFINITA DI UN CONTENUTO}.
+
+let acquaLiscia = 20;
+
+
+
+
+// CIOTOLA CON AL SUO INTERNO NIENTE {VARIABILE DICHIARATA MA NON DEFINITA DI UN CONTENUTO }.
+let acquaFrizzante =;
+
+
+
+// IL "NULL" SVUOTA LA CIOTOLA DEL SUO CONTENUTO.
+let acquaLiscia = null;
+
+// NULL & UNDEFINED non sono la stesa cosa!
